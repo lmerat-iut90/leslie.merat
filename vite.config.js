e2c8fa3807529b8c7257842a096232fcdc9f928d
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2025-11-30 00:22:48 +0100

    deploy !!!

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/leslie.merat.github.io/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
